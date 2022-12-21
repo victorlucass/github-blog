829eaaa8e3ef11f960b49e8b9ab433e36ce76f47
Victor Lucas A. de Oliveira <59880314+victorlucass@users.noreply.github.com>
2022-12-21 14:27:00 -0400

    feat: add theme and type theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import { RoutesApp } from "./Routes";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/theme";
 
 function App() {
   return (
     <BrowserRouter>
-      <RoutesApp></RoutesApp>
+      <ThemeProvider theme={defaultTheme}>
+        <RoutesApp></RoutesApp>
+      </ThemeProvider>
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
