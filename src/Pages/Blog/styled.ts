a745cf588f648416be798654a0826da698b4596e
Victor Lucas A. de Oliveira <59880314+victorlucass@users.noreply.github.com>
2022-12-21 17:07:59 -0400

    feat: add link in info

--- src/Pages/Blog/styled.ts
+++ src/Pages/Blog/styled.ts
@@ -13,6 +13,7 @@ export const CardInfoName = styled.div`
   margin-bottom: 8px;
 `
 export const LinkGitHubStyle = styled.a`
+  align-self: flex-start;
   font-style: normal;
   font-weight: 700;
   line-height: 160%;
@@ -24,6 +25,9 @@ export const LinkGitHubStyle = styled.a`
   &,
   i {
     font-size: ${(props) => props.theme.textSizes['textSize-12']};
+  }
+  &:hover {
+    border-bottom: 1px solid ${(props) => props.theme.colors.blue};
   }
 `
 
